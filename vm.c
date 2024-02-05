97964fe00779e9721a1d2139e39a93f271b5203a
Brandon5497 <89703971+Brandon5497@users.noreply.github.com>
2024-02-05 15:05:06 -0500

    Update vm.c

--- vm.c
+++ vm.c
@@ -18,6 +18,12 @@ int base(int BP, int L) //Find base L levels down function
     }
     return arb;
 }
+typedef struct instructionRegister //struct to store the values of the instruction register
+{
+    int OP;
+    int L;
+    int M;
+}instructionRegister;
 
 
 int main(int argc, char *fileName[]){
@@ -26,11 +32,12 @@ int main(int argc, char *fileName[]){
     int flag = 1;//EOP flag
     int curVal; //current value in file input
     int pc = 0;//program counter
-    int curpc = 0;//program counter for execution(for how we wrote it, the ISA statements are the same)
+    //int curpc = 0;//program counter for execution(for how we wrote it, the ISA statements are the same)
     int sp = 500; //stack pointer
     int bp = sp - 1; //base pointer
     int bpArr[10] = {499};//base pointer array for printing functionality
-    int bpCount = 0;//count number of bp changes for printing functionality    
+    int bpCount = 0;//count number of bp changes for printing functionality 
+    instructionRegister IR;
     FILE* inputFile = fopen(fileName[1], "r"); //initialize file pointer & text input file in read mode
     while(fscanf(inputFile, "%d", &curVal) == 1)//loop through text and insert in PAS
     {
@@ -41,29 +48,31 @@ int main(int argc, char *fileName[]){
     printf("Initial values:  %d %d %d\n", pc, bp, sp); 
     while(flag == 1)//fetch loop
     {
-        curpc = pc;//to keep track of index for current execution
+        IR.OP = pc;//tracks the OP component in IR register
+        IR.M = pc + 2;//tracks the M component in IR register
+        IR.L = pc + 1;//tracks the L component in IR register
         pc = pc + 3;//actual program counter
-        switch(pas[curpc])//checking instruction type
+        switch(pas[IR.OP])//checking instruction type
 
         {
 
             case 1: //LIT 0,M
             {
                 sp -= 1;
-                pas[sp] = pas[curpc + 2];
-                printf("LIT %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                pas[sp] = pas[IR.M];
+                printf("LIT %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                 break;
             }
             case 2: //OPR 0,M
             {
-                switch(pas[curpc + 2])//math opr or RTN check
+                switch(pas[IR.M])//math opr or RTN check
                 {
                     case 0://RTN
                     {
                     sp = bp + 1;
                     bp = pas[sp - 2];
                     pc = pas[sp - 3];
-                    printf("RTN %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                    printf("RTN %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                     bpCount--;
                     break;
                     }
@@ -71,76 +80,76 @@ int main(int argc, char *fileName[]){
                     {
                         pas[sp + 1] = pas[sp + 1] + pas[sp];
                         sp += 1;
-                        printf("ADD %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("ADD %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 2://SUB
                     {
                         pas[sp + 1] = pas[sp + 1] - pas[sp];
                         sp += 1;
-                        printf("SUB %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("SUB %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 3://MUL
                     {
                         pas[sp + 1] = pas[sp + 1] * pas[sp];
                         sp += 1;
-                        printf("MUL %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("MUL %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 4://DIV
                     {
                         pas[sp + 1] = pas[sp +1] / pas[sp];
                         sp += 1;
-                        printf("DIV %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("DIV %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 5://EQL
                     {
                         pas[sp +1] = pas[sp + 1] == pas[sp];
                         sp += 1;
-                        printf("EQL %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("EQL %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 6://NEQ
                     {
                         pas[sp + 1] = pas[sp + 1] != pas[sp];
                         sp += 1;
-                        printf("NEQ %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("NEQ %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 7://LSS
                     {
                         pas[sp + 1] = pas[sp + 1] < pas[sp];
                         sp += 1;
-                        printf("LSS %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("LSS %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 8://LEQ
                     {
                         pas[sp + 1] = pas[sp + 1] <= pas[sp];
                         sp += 1;
-                        printf("LEQ %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("LEQ %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 9://GTR
                     {
                         pas[sp + 1] = pas[sp + 1] > pas[sp];
                         sp += 1;
-                        printf("GTR %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("GTR %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 10://GEQ
                     {
                         pas[sp + 1] = pas[sp + 1] >= pas[sp];
                         sp += 1;
-                        printf("GEQ %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("GEQ %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 11://ODD
                     {
                         pas[sp] =  pas[sp] % 2;
-                        printf("ODD %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("ODD %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                 }
@@ -150,64 +159,64 @@ int main(int argc, char *fileName[]){
             case 3: //LOD L,M
             {
                 sp -= 1;
-                pas[sp] = pas[base(bp, pas[curpc + 1]) - pas[curpc + 2]];
-                printf("LOD %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                pas[sp] = pas[base(bp, pas[IR.L]) - pas[IR.M]];
+                printf("LOD %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                 break;
             }
             case 4: //STO L<M
             {
-                pas[base(bp, pas[curpc + 1]) - pas[curpc + 2]] = pas[sp];
+                pas[base(bp, pas[IR.L]) - pas[IR.M]] = pas[sp];
                 sp += 1;
-                printf("STO %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                printf("STO %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                 break;
             }
             case 5: //CAL L,M | Initializing new Activation Record
             {
-                pas[sp - 1] = base(bp, pas[curpc + 1]); //static link (SL)
+                pas[sp - 1] = base(bp, pas[IR.L]); //static link (SL)
                 pas[sp - 2] = bp; //dynamic link (DL)
                 pas[sp - 3] = pc; //return address (RA)
                 bp = sp - 1; //base pointer points to static link
-                pc = pas[curpc + 2]; //M value
-                printf("CAL %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                pc = pas[IR.M]; //M value
+                printf("CAL %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                 bpCount++;
                 bpArr[bpCount] = bp;
                 break;
             }
             case 6: //INC 0,M
             {
-                sp -= pas[curpc + 2];
-                printf("INC %d %d\t\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                sp -= pas[IR.M];
+                printf("INC %d %d\t\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                 break;
             }
             case 7: //JMP 0,M
             {
-                printf("JMP %d %d\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pas[curpc + 2], bp, sp);
-                pc = pas[curpc + 2];
+                printf("JMP %d %d\t %d %d %d\t", pas[IR.L], pas[IR.M], pas[IR.M], bp, sp);
+                pc = pas[IR.M];
                 break;
             }
             case 8: //JPC 0,M
             {
                 if (pas[sp] == 0)
                 {
-                    pc = pas[curpc + 2];
+                    pc = pas[IR.M];
                     sp += 1;
                 }
                 else
                 {
                     sp += 1;
                 }
-                printf("JPC %d %d\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                printf("JPC %d %d\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                 break;
             }
             case 9: //SYS (Need to include three subroutines)
             {
-                switch(pas[curpc + 2])
+                switch(pas[IR.M])
                 {
                     case 1:
                     {
                         printf("Output result is: %d\n", pas[sp]);
                         sp += 1;
-                        printf("SOU %d %d\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("SOU %d %d\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                     case 2:
@@ -220,7 +229,7 @@ int main(int argc, char *fileName[]){
                     case 3:
                     {
                         flag = 0;
-                        printf("EOP %d %d\t %d %d %d\t", pas[curpc + 1], pas[curpc + 2], pc, bp, sp);
+                        printf("EOP %d %d\t %d %d %d\t", pas[IR.L], pas[IR.M], pc, bp, sp);
                         break;
                     }
                 }
